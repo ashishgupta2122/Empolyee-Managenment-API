1fbfbe794fe137313ba7cd5e106cf826cc1068e3
Ashish Gupta <105555634+ashishgupta2122@users.noreply.github.com>
2025-08-07 11:27:25 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,7 +11,7 @@ const authMiddleware = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("🔑 Token decoded:", decoded);  // ADD THIS 🔥
+        console.log("Token decoded:", decoded);  // ADD THIS 
         req.user = decoded;
         next();
     } catch (err) {
